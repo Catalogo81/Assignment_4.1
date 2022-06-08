93182ecf85730b3f63f79f7029b98b5e5ee4893b
Catalogo81 <peacematjato@gmail.com>
2022-06-08 23:08:09 +0200

    Latest commit

--- src/MyException.java
+++ src/MyException.java
@@ -1,7 +1,17 @@
 
 //Question 4: Below, write an unchecked Exception class MyException.
-public class MyException
+public class MyException extends Exception
 {
+    private int result;
+    MyException(int exeption)
+    {
+        result = exeption;
+    }
+    public String toString()
+    {
+        return "MyException[" + result + "]";
+    }
+
     public static void main(String[] args)
     {
         int number = Integer.parseInt(null);
--- src/Question_6.java
+++ src/Question_6.java
@@ -4,8 +4,66 @@ public class Question_6
 {
     static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args)
+    public static void main(String args[])
     {
+        try
+        {
+            int number = scanner.nextInt();
+            int[] array = new int[number];
+            scanner.nextLine();
+            for(int i = 0; i < number; i++)
+            {
+                array[i]=Integer.parseInt(scanner.nextLine());
+            }
 
+            String string = scanner.nextLine();
+            System.out.println(string.charAt(10));
+
+            solution(array);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Invalid division");
+        }
+        catch(NumberFormatException e)
+        {
+            System.out.println("Format mismatch");
+        }
+        catch(StringIndexOutOfBoundsException e)
+        {
+            System.out.println("Index is invalid");
+        }
+
+        catch(ArrayIndexOutOfBoundsException e)
+        {
+            System.out.println("Array index is invalid");
+        }
+        catch(MyException e)
+        {
+            System.out.println(e);
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception encountered");
+        }
+        finally
+        {
+            System.out.println("Exception Handling Completed");
+        }
+
+
+    }
+    static void solution(int[] array) throws Exception
+    {
+        int answer = 0;
+
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = i + 1; j < 10; j++)
+            {
+                answer += array[i] / array[j];
+            }
+        }
+        throw new MyException(answer);
     }
 }
